e18a8f36987bc61654aed80cc52e41f096df65a6
lxj <lxj@tomtaw.com.cn>
2025-08-14 22:32:30 +0800

    调整项目结构

---
+++ Tic-Tac-Toe.Server/Tic-Tac-Toe.Server/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tic_Tac_Toe.Server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+    }
+}
